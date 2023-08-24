55b495e26c796d8b636cc222117b5abeb3d27f2f
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2023-08-24 15:47:37 -0300

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: %i[show edit update destroy]
 
   def index
     @articles = Article.all
@@ -14,36 +14,33 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    if @article.save
-      redirect_to @article, notice: 'Article was successfully created.'
-    else
-      render :new
-    end
+    @article.save
+
+    redirect_to article_path(@article)
   end
 
   def edit
   end
 
   def update
-    if @article.update(article_params)
-      redirect_to @article, notice: 'Article was successfully updated.'
-    else
-      render :edit
-    end
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
   end
 
   def destroy
     @article.destroy
-    redirect_to articles_url, notice: 'Article was successfully destroyed.'
+
+    redirect_to articles_path
   end
 
   private
 
-  def set_article
-    @article = Article.find(params[:id])
-  end
-
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
